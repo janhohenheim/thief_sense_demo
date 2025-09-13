869894eb9085cb5f606a4198494ac27efc541d8d
Jan Hohenheim <jan@hohenheim.ch>
2025-09-13 20:18:59 +0200

    Move to path corner when clicking on AI

--- src/demo/click.rs
+++ src/demo/click.rs
@@ -1,5 +1,13 @@
-use bevy::{color::palettes::tailwind, picking::pointer::PointerInteraction, prelude::*};
+use bevy::{
+    color::palettes::tailwind, ecs::relationship::Relationship as _,
+    picking::pointer::PointerInteraction, prelude::*,
+};
 use bevy_landmass::{AgentTarget3d, Archipelago3d, FromAgentRadius as _, PointSampleDistance3d};
+
+use crate::{
+    demo::{npc::Npc, path_corner::PathCorner, target::Target},
+    third_party::landmass::AgentOf,
+};
 
 pub(super) fn plugin(app: &mut App) {
     app.add_systems(Update, draw_mesh_intersections);
@@ -19,17 +27,29 @@ fn draw_mesh_intersections(pointers: Query<&PointerInteraction>, mut gizmos: Giz
 
 fn on_click(
     click: Trigger<Pointer<Click>>,
-    mut agents: Query<&mut AgentTarget3d>,
+    mut agents: Query<(&AgentOf, &mut AgentTarget3d)>,
     archipelago: Single<&Archipelago3d>,
+    npcs: Query<(), With<Npc>>,
+    meshes: Query<(), With<Mesh3d>>,
+    path_corners: Query<Entity, With<PathCorner>>,
+    mut commands: Commands,
 ) {
     let Some(point) = click.hit.position else {
         return;
     };
-    let sampled_point =
-        archipelago.sample_point(point, &PointSampleDistance3d::from_agent_radius(5.0));
-    if let Ok(sampled_point) = sampled_point {
-        for mut target in &mut agents {
-            *target = AgentTarget3d::Point(sampled_point.point());
+    if meshes.contains(click.target()) {
+        let sampled_point =
+            archipelago.sample_point(point, &PointSampleDistance3d::from_agent_radius(5.0));
+        if let Ok(sampled_point) = sampled_point {
+            for (agent_of, mut target) in &mut agents {
+                commands.entity(agent_of.get()).remove::<Target>();
+                *target = AgentTarget3d::Point(sampled_point.point());
+            }
         }
+    } else if npcs.contains(click.target()) {
+        let arbitrary_path_corner = path_corners.iter().next().unwrap();
+        commands
+            .entity(click.target())
+            .insert(Target(arbitrary_path_corner));
     }
 }
