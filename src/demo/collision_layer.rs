178fff1a5c02344f85678ad30e6b13a870a128e2
Jan Hohenheim <jan@hohenheim.ch>
2025-09-29 01:26:34 +0200

    Impl exposure

--- src/demo/collision_layer.rs
+++ src/demo/collision_layer.rs
@@ -1,8 +1,14 @@
-use avian3d::prelude::PhysicsLayer;
+use avian3d::prelude::*;
 use bevy::prelude::*;
+use bevy_trenchbroom::geometry::Brushes;
 
 pub(super) fn plugin(app: &mut App) {
-    let _ = app;
+    app.register_required_components_with::<Brushes, _>(|| {
+        CollisionLayers::new(
+            [CollisionLayer::Default, CollisionLayer::Brush],
+            [CollisionLayer::Default],
+        )
+    });
 }
 
 #[derive(Debug, PhysicsLayer, Default)]
@@ -12,4 +18,5 @@ pub(crate) enum CollisionLayer {
     AiVisible,
     LightSource,
     Transparent,
+    Brush,
 }
--- src/demo/visibility.rs
+++ src/demo/visibility.rs
@@ -1,7 +1,13 @@
-use avian3d::prelude::LinearVelocity;
-use bevy::prelude::*;
+use std::f32::consts::TAU;
 
-use crate::rand_timer::RandTimer;
+use avian3d::prelude::*;
+use bevy::prelude::*;
+use bevy_trenchbroom::geometry::Brushes;
+
+use crate::{
+    demo::{collision_layer::CollisionLayer, player::PLAYER_RADIUS},
+    rand_timer::RandTimer,
+};
 
 pub(super) fn plugin(app: &mut App) {
     // NOT calling `add_rand_timer` because we want to manually reset it
@@ -32,19 +38,37 @@ pub(crate) struct AiVisibility {
 pub(crate) fn get_or_update_visibility(
     In(entity): In<Entity>,
     mut timers: Query<(&mut AiVisibility, &mut VisibilityTimer)>,
-    object: Query<&LinearVelocity>,
+    object: Query<(&GlobalTransform, &Collider, &LinearVelocity)>,
+    spatial: SpatialQuery,
 ) -> Result<AiVisibility> {
     let (mut visibility, mut timer) = timers.get_mut(entity)?;
     if !timer.is_finished() {
         return Ok(*visibility);
     }
-    let velocity = object.get(entity)?;
+    let (transform, collider, velocity) = object.get(entity)?;
+    let translation = transform.translation();
+    const TRIES: u8 = 8;
+    // This distance is equivalent to "infinitely far away"
+    const MAX_WALL_DISTANCE: f32 = 5.0;
+    let mut closest_wall = MAX_WALL_DISTANCE;
+    for i in 0..TRIES {
+        let dir = Quat::from_rotation_y(i as f32 * TAU / TRIES as f32) * Dir3::NEG_Z;
+        let hit = spatial.cast_ray(
+            translation,
+            dir,
+            MAX_WALL_DISTANCE,
+            true,
+            &SpatialQueryFilter::from_mask([CollisionLayer::Brush]),
+        );
+        if let Some(hit) = hit {
+            closest_wall = closest_wall.min(hit.distance);
+        }
+    }
     *visibility = AiVisibility {
         // TODO: do some raycasts. Can probably first do an AABB or sphere check to gather the potential light sources. Also remember to filter out `CollisionLayer::Transparent` from raycasts.
         lighting: 0.0,
         movement: velocity.length(),
-        // TODO: use https://docs.rs/avian3d/latest/avian3d/collision/collider/contact_query/fn.distance.html
-        exposure: 0.0,
+        exposure: closest_wall,
     };
     // Since this system is not called every frame, but only for entities that are currently looked at by AI,
     // we only reset the timer when necessary.
