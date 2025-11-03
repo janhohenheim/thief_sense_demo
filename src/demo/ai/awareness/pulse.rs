7a739e5057fb5035d504f30c265e33aec9f9c6b7
Jan Hohenheim <jan@hohenheim.ch>
2025-11-03 07:22:07 +0100

    Fix flush

--- src/demo/ai/awareness/pulse.rs
+++ src/demo/ai/awareness/pulse.rs
@@ -125,7 +125,7 @@ pub(crate) fn pulse(
         awareness.capacitor.reset();
     }
     awareness_query.get_mut(world).set(npc, object, awareness);
-    world.flush();
+    awareness_query.apply(world);
     Ok(())
 }
 
